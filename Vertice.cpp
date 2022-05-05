9d7ebc8822ac936106eb1716fec169896eacee9d
kedoshim <abraaodpaula@gmail.com>
2022-05-05 10:47:01 -0300

    Criado o básico das funções de Vertice

--- Vertice.cpp
+++ Vertice.cpp
@@ -1,1 +1,25 @@
+Vertice::Vertice(int id,float p)
+{
+    ID=id;
+    setPeso(p);
+}
+Vertice::Vertice(int id,float p, Vertice* anterior)
+{
+    ID=id;
+    setPeso(p);
+    setAnterior(anterior);
 
+}
+Vertice::setProximo(Vertice* p)
+{
+    vproximo=p;
+}
+Vertice::setAnterior(Vertice* a)
+{
+    vanterior=a;
+    a.setProximo(this);//preciso do endereço do Vertice que estou trabalhando agora. n sei c ta certo
+}
+Vertice::setPeso(float p)
+{
+    peso=p;
+}
