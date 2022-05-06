7a0172adb3bc99fcea5b1e4cdf5c7d255f26e87e
kedoshim <abraaodpaula@gmail.com>
2022-05-05 21:31:52 -0300

    Adicionadas novas funções à Grafo

--- Grafo.cpp
+++ Grafo.cpp
@@ -4,10 +4,29 @@ Grafo::Grafo(int nvert,int vPonderados,int aPonderadas)
     int id;
     vert1=new Vertice(1);
     first=vert1;
-    for(id=2;id<=nvert;id++){
+    for(id=2;id<=nvert/2;id++){   //fiz a lista de vertices como um ciclo duplamente encadeado
         vert2=new Vertice(id);
         vert1.setProximo(vert2);
         vert2.setAnterior(vert1);
         vert1=vert2;
     }
+    vert1.setProximo(first);
+    first.setAnterior(vert1);
+    vatual=vert1;
+}
+
+Grafo::getVerticeDeID(int id)
+{
+    Vertice* v;
+    v=first;
+    if(id<nvert/2)
+        for(v=first;id!=v.getID();v=v.getProximo())
+    else
+        for(v=first;id!=v.getID();v=v.getAnterior())
+    return v;
+}
+
+Grafo::getArestaEntreAB(int IDvertA,int IDvertB)
+{
+
 }
--- Grafo.h
+++ Grafo.h
@@ -5,8 +5,12 @@ class Grafo
 {
 private:
     int nvertices;
+    Vertice* first;
 public:
     Grafo(int nVert, int vPonderados, int aPonderadas);//1 se ponderado, 0 se não
     ~Grafo();
-
+    setAresta(int IdA,int idB,float peso,int direcionado);
+    setPesoVertice(int id, float peso);
+    Aresta* getArestaEntreAB(int IDvertA,int IDvertB);
+    Vertice* getVerticeDeID(int id);
 };
