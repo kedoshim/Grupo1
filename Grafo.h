9d6a98342ab26e09b272cf7d9dae43cf8a60c3d8
kedoshim <abraaodpaula@gmail.com>
2022-05-08 00:15:17 -0300

    adicionado tipo de retorno à algumas funções de Grafo

--- Grafo.h
+++ Grafo.h
@@ -11,8 +11,8 @@ private:
 public:
     Grafo(int nVert, int aPonderadas, int vPonderados, int direcionado);//1 se ponderado, 0 se não
     ~Grafo();
-    setAresta(int IdA,int idB,float peso,int direcionado);
-    setPesoVertice(int id, float peso);
+    void setAresta(int IdA,int idB,float peso,int direcionado);
+    void setPesoVertice(int id, float peso);
     Aresta* getArestaEntreAB(int IDvertA,int IDvertB);
     Vertice* getVerticeDeID(int id);
 };
