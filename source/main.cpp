e738a382a74c1926b86a16f09bbfe51852a8c25d
jooj211 <jdmctuber@gmail.com>
2022-05-21 13:32:54 -0300

    mudando configurações de debug e arrumando parâmetros dos argumentos na main

--- source/main.cpp
+++ source/main.cpp
@@ -6,15 +6,16 @@ using namespace std;
 
 int main(int argc, char **argv) /*argv[1]=input; argv[2]=output; argv[3]==grafo direcionado; argv[4]==aresta ponderada; argv[5]==vertice ponderado*/
 {
-  if(argc!=6){
+  if (argc != 6)
+  {
     cout << "Numero errado de argumentos" << endl;
     cout << "Favor executar do seguinte modo:" << endl;
-    cout << "./Grupo1 'arquivo de entrada' 'arquivo de saida' 'O grafo é direcionado?(0/1)' 'As arestas são ponderadas?(0/1)' 'Os vertices são ponderados?(0/1)'"  << endl;
+    cout << "./Grupo1 'arquivo de entrada' 'arquivo de saida' 'O grafo eh direcionado?(0/1)' 'As arestas sao ponderadas?(0/1)' 'Os vertices sao ponderados?(0/1)'" << endl;
     return 1;
   }
 
   ifstream arquivo;
-  arquivo.open(*argv[1]);
+  arquivo.open(argv[1]);
 
   int nvertices;
   int id;
@@ -25,8 +26,7 @@ int main(int argc, char **argv) /*argv[1]=input; argv[2]=output; argv[3]==grafo 
   Vertice *vert2;
 
   cin >> nvertices;
-  grafo = new Grafo(nvertices, *argv[4], *argv[5], *argv[3]);
-
+  grafo = new Grafo(nvertices, stoi(argv[4]), stoi(argv[5]), stoi(argv[3])); //argumentos precisam ser int
 
   return 0;
 }
